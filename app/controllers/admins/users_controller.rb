1978389a39d1f7dba3d062a03a7b245de46762d0
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-28 20:00:42 +0800

    [Feature 1] Generate devise admin(views and controller)

---
+++ app/controllers/admins/users_controller.rb
@@ -0,0 +1,12 @@
+class Admins::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_admin
+
+  def index
+    @users = User.all
+  end
+
+  def check_admin
+    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
+  end
+end
