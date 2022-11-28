7a8212c897affecca69fa0b52ef5c9c43b0f3996
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-28 20:07:33 +0800

    [Feature 1] Add routes routes for admin and client

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
 
   constraints(ClientDomainConstraint.new) do
-    resources :home
+    root to: 'home#index'
+    devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   end
 
   constraints(AdminDomainConstraint.new) do
-    namespace :admin do
-      resources :users
+    namespace :admins, path: '' do
+      root to: 'home#index'
+      devise_for :users, controllers: { sessions: 'admins/sessions' }
     end
   end
 end
