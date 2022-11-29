62977ab6b55ad0cabaf88c95cbd048a0411b41f6
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-29 20:30:54 +0800

    remove uneccessary files and fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   constraints(ClientDomainConstraint.new) do
-    root to: 'home#index'
+    root to: 'homes#index'
     devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-    resources :home
+    resources :homes
+    resource :me
   end
 
   constraints(AdminDomainConstraint.new) do
