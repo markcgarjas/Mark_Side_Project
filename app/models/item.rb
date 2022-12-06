8337c0cd3c14afd4a4aa0af7459dd2c0543f7431
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-12-06 14:22:57 +0800

    [Feature 11] Item soft delete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
+
   validates :image, presence: true
   validates :name, presence: true
   validates :minimum_bets, presence: true
@@ -10,4 +12,8 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   enum status: { Inactive: 0, Active: 1 }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_032601) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_055612) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_032601) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
