e3d5692597f3bbfc563cb076b2df8ffed4c9f21a
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-12-16 11:53:44 +0800

    [Feature 23]  Client Profile Page

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,5 +1,9 @@
 class Users::ProfilesController < ApplicationController
   def show
     @user = current_user
+    @orders = Order.where(user: @user) if params[:order] == 'orders'
+    @winners = Winner.where(user: @user) if params[:winner] == 'winners'
+    @lotteries = Bet.where(user: @user) if params[:lottery] == 'lotteries'
+    @invitations = User.where(parent: @user) if params[:invitation] == 'invitations'
   end
 end
