416c2bdafb5837902b8eedf1fdf2598c089c9cbe
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-12-20 19:58:21 +0800

    [Feature 35] Client Homepage view

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,5 +1,8 @@
 class Users::HomesController < ApplicationController
   before_action :authenticate_users_user!, except: :index
 
-  def index; end
+  def index
+    @winners = Winner.includes(:user).published.limit(5)
+    @items = Item.active.starting.limit(5)
+  end
 end
